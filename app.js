fe3fa14bba14cf1a9af65ea503d6764c72d2af2b
SBastos800 <soniavsbastos@gmail.com>
2020-10-27 12:16:35 +0000

    response.on with all the chosen parametres: temp, description, minTemp, maxTemp and humidity

--- app.js
+++ app.js
@@ -8,7 +8,18 @@ app.get('/', (req, res) => {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=66538826eb71262703743008a9fcb91b';
     
     https.get(url, (response) => {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on('data', (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const minTemp = weatherData.main.temp_min;
+            const maxTemp = weatherData.main.temp_max;
+            const humidity = weatherData.main.humidity;
+            console.log(temp);
+
+        });
     });
     
     res.send("Hello");
