ba9e270944f43dc9755bb9cd5cbd72186da370bb
SBastos800 <soniavsbastos@gmail.com>
2020-10-27 12:44:51 +0000

    added icon image of the weather condition

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
 
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=66538826eb71262703743008a9fcb91b';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=66538826eb71262703743008a9fcb91b&units=metric';
     
     https.get(url, (response) => {
         console.log(response.statusCode);
@@ -17,12 +17,18 @@ app.get('/', (req, res) => {
             const minTemp = weatherData.main.temp_min;
             const maxTemp = weatherData.main.temp_max;
             const humidity = weatherData.main.humidity;
-            console.log(temp);
-
+            const icon = weatherData.weather[0].icon;
+            res.write(`<h1>The temperature in London is ${temp} degrees celsius.</h1>`);
+            res.write(`<h3>The weather is currently ${description}</h3>`);
+            res.write(`<ul>
+                <li>Minimum Temperature: ${minTemp}&deg</li>
+                <li>Maximum Temperature: ${maxTemp}&deg</li>
+                <li>Humidity: ${humidity}</li>
+                </ul>`);
+            res.write(`<img src='http://openweathermap.org/img/wn/${icon}@2x.png'/>`);
+            res.send();
         });
     });
-    
-    res.send("Hello");
 });
 
 app.listen(3000, () => {
