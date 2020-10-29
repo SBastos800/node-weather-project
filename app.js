954964ad4cf520ea3a0b3a33bec92e5a14b481ae
SBastos800 <soniavsbastos@gmail.com>
2020-10-29 21:50:58 +0000

    Added css to the form screen

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
